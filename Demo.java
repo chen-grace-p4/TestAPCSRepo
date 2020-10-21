405b1993ae240e6b777a359ca1860c81a37c3a87
Grace <gchen20@stuy.edu>
2020-10-21 19:58:29 -0400

    I added the two methods from class

--- Demo.java
+++ Demo.java
@@ -1,12 +1,18 @@
 public class Demo {
   public static void main(String[]args) {
-    if (args.length == 0) {
-      printLoop(5);
-    } else {
-      int a = Integer.parseInt(args[0]);
-      printLoop(a);
-    }
-
+    // if (args.length == 0) {
+    //   printLoop(5);
+    // } else {
+    //   int a = Integer.parseInt(args[0]);
+    //   printLoop(a);
+    // }
+    int[] a = {1, 2, 3};
+    int[] b = {1, 2, 3, 4};
+    int[] c = {1, 2};
+    int[] d = {};
+    int[] e = {1, 2, 3, 4, 5};
+    int[][] all = {a, b, c, d, e};
+    System.out.print(arrayDeepToString(all).replace("}, ","},\n "));	
   }
   public static void printLoop(int n){
       int times  = n;
@@ -17,5 +23,28 @@ public class Demo {
       times = times - 1;
       System.out.println();
     }
+  }
+
+  public static String arrToString(int[] arr) {
+    String newstring = "{";
+    for (int i=0; i<arr.length; i++) {
+      newstring += arr[i];
+      if (arr.length > 1 && i < arr.length-1) {
+        newstring += ", ";
+        }
+    }
+    newstring += "}";
+    return newstring;
+  }
+  public static String arrayDeepToString(int[][] arr) {
+    String newstring2 = "{";
+    for (int overall = 0; overall < arr.length; overall++) {
+        newstring2 += arrToString(arr[overall]);
+        if (arr.length > 1 && overall < arr.length-1) {
+          newstring2 += ", ";
+        }
+    }
+    newstring2 += "}";
+    return newstring2;
   }
 }
